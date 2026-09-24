55045d477b66b637cafff2a9b36d1cd7d50bb13a
Artem Ishchenko <nordeptanah8@gmail.com>
2026-09-25 00:00:49 +0300

    initial project setup, dependencies, base styles and folder and file structure

---
+++ src/js/constants.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://wedding-photographer.b.goit.study/api';
+
+export const ENPOINTS = {
+  categoties: '/categories',
+  orders: '/orders',
+  photos: '/wedding-photos',
+  feedbacks: '/feedbacks',
+};
