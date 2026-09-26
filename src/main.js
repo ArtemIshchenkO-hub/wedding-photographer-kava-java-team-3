c94e6883a8086751f905f92853c636c34492e0b2
Mariia Shev <mariia.shevchenkoooo@gmail.com>
2026-09-26 17:53:01 +0300

    Add scroll button disappearance

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-
 import './js/header.js';
 import './js/success-modal.js';
-import { scrollUpBtn, scrollUp } from './js/scroll-up.js';
+import { scrollUpBtn, scrollUp, scrollFunction } from './js/scroll-up.js';
 
+window.onscroll = function () {
+  scrollFunction();
+};
 scrollUpBtn.addEventListener('click', scrollUp);
-
